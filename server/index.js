97d4594fb3ad3cd3ee147ec37862a297d06e68b7
MAHAK <mahak191713@gmail.com>
2024-06-10 20:03:40 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io  = socket(server,{
     cors:{
-        origin:"https://chat-application-lovat-ten.vercel.app",
+        origin:"https://chat-application-lovat-ten.vercel.app/login",
         credentials:true,
     }
 });
