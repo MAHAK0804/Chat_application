2251a178f211e9c12749e7dc8e03d04641bdc070
MAHAK <mahak191713@gmail.com>
2024-06-10 23:57:59 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const userRoutes = require("./routes/usersRoutes");
 const messageRoute = require("./routes/mesaagesRoute");
+const { createServer } = require('http');
 
 const app = express();
 const socket = require('socket.io');
@@ -14,13 +15,16 @@ app.use("/api/auth", userRoutes);
 app.use("/api/messages", messageRoute);
 
 
-mongoose.connect('mongodb+srv://mahakdeveloper0804:Mahak%40080204@cluster0.lpbdmkv.mongodb.net/chat_app'
-);
+mongoose.connect('mongodb+srv://mahakdeveloper0804:Mahak%40080204@cluster0.lpbdmkv.mongodb.net/chat_app',
+                  { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
-const server = app.listen(5000, () => {
-    console.log(`Server is started on PORT:5000`);
-});
 
+// const server = app.listen(5000, () => {
+//     console.log(`Server is started on PORT:5000`);
+// });
+const httpServer = createServer(app);
 const io  = socket(server,{
     cors:{
         origin:"https://chat-application-lovat-ten.vercel.app",
@@ -30,16 +34,27 @@ const io  = socket(server,{
 });
 global.onlineUsers = new Map();
 
-io.on("connection", (socket)=>{
-    global.chatSocket = socket;
-    socket.on("add-user",(userID)=>{
-        onlineUsers.set(userID,socket.id);
+io.on("connection", (socket) => {
+    console.log('New WebSocket connection:', socket.id);
+
+    socket.on("add-user", (userID) => {
+        onlineUsers.set(userID, socket.id);
+        console.log('User added:', userID);
     });
-    socket.on("send-msg",(data)=>{
+
+    socket.on("send-msg", (data) => {
         const sendUserSocket = onlineUsers.get(data.to);
-        if(sendUserSocket)
-            {
-                socket.to(sendUserSocket).emit("msg-recieve",data.message);
-            }
+        if (sendUserSocket) {
+            socket.to(sendUserSocket).emit("msg-recieve", data.message);
+            console.log('Message sent to:', data.to);
+        }
     });
-})
+
+    socket.on("disconnect", () => {
+        console.log('WebSocket disconnected:', socket.id);
+    });
+});
+
+httpServer.listen(process.env.PORT, () => {
+    console.log(`Server is started on PORT: ${process.env.PORT}`);
+});
