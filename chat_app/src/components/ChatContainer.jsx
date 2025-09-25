ed34659c3261f59aa0fa7b7eaa1e213491822954
MAHAK <mahak191713@gmail.com>
2025-09-25 17:34:18 +0530

    Update ChatContainer.jsx

--- chat_app/src/components/ChatContainer.jsx
+++ chat_app/src/components/ChatContainer.jsx
@@ -72,7 +72,7 @@ export default function ChatContainer({ currentChat, currentUser,socket }) {
                     <div className="chat-header">
                         <div className="user-details">
                             <div className="avatar">
-                                <img src={`data:image/svg+xml;base64,${currentChat.avatarImage}`} alt="avatar" />
+                                <img src={`${currentChat.avatarImage}`} alt="avatar" />
                             </div>
                             <div className="username">
                                 <h3>{currentChat.username}</h3>
