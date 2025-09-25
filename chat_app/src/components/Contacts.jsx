e08763a063f3f1dac5f5faecf040dbd22ffdf1ec
MAHAK <mahak191713@gmail.com>
2025-09-25 17:19:28 +0530

    Update Contacts.jsx

--- chat_app/src/components/Contacts.jsx
+++ chat_app/src/components/Contacts.jsx
@@ -37,7 +37,7 @@ export default function Contacts({ contacts, currentUser, changeChat }) {
                                         <div className={`contact ${index === currentSelected ? "selected" : " "}`}
                                             key={index} onClick={() => changeCurrentChat(index, contact)} >
                                             <div className="avatar">
-                                                <img src={`data:image/svg+xml;base64,${contact.avatarImage}`} alt="avatar" />
+                                                <img src={`${contact.avatarImage}`} alt="avatar" />
                                             </div>
                                             <div className="username">
                                                 <h3>{contact.username}</h3>
@@ -51,7 +51,7 @@ export default function Contacts({ contacts, currentUser, changeChat }) {
                         </div>
                         <div className="current-user">
                             <div className="avatar">
-                                <img src={`data:image/svg+xml;base64,${currentUserImage}`} alt="avatar" />
+                                <img src={`${currentUserImage}`} alt="avatar" />
                             </div>
                             <div className="username">
                                 <h3>{currentUserName}</h3>
